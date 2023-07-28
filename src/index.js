32e40a8767db19114164ec4549febf585ced5068
Genesis Scott <chandlerscott123@gmail.com>
2023-07-27 21:56:40 -0500

    implement UI WIP: accurately display method output

--- src/index.js
+++ src/index.js
@@ -3,26 +3,35 @@ import './css/styles.css';
 import 'bootstrap';
 
 
+
 function handleYearsBetween(event) {
   event.preventDefault();
   document.querySelector('div#years-between').classList.remove('hidden');
   const birthday1 = document.querySelector('#birthday1').value;
   const birthday2 = document.querySelector('#birthday2').value;
-  const person = new Person(document.querySelector('#name').value, document.querySelector('#age').value);
+  const person = new Person(document.querySelector('#name').value, parseInt(document.querySelector('#age').value));
   let ages = person.yearsBetween(birthday1, birthday2);
-  ages = Object.entries(ages);
-  ages.forEach(function (age) {
-    document.querySelector(`#${age[0]}`).innerHTML = age[1];
+  ages = {
+    'mercury': ages[1],
+    'venus': ages[2],
+    'mars': ages[3],
+    'jupiter': ages[4]
+  };
+  Object.entries(ages).forEach(function ([planet, age]) {
+    document.querySelector(`span#${planet}Between`).innerHTML = age;
   });
+
+  document.querySelector(`span#birthday1`).innerHTML = birthday1;
+  document.querySelector(`span#birthday2`).innerHTML = birthday2;
+  document.querySelector('ul#years-between').classList.remove('hidden');
 }
 
 function handleYearsUntil(event) {
   event.preventDefault();
   document.querySelector('div#years-until').classList.remove('hidden');
-  document.querySelector('ul#years-until').classList.remove('hidden');
-  const birthday = document.querySelector('#birthday').value;
+  const futurebirthday = document.querySelector('#futurebirthday').value;
   const person = new Person(document.querySelector('#name').value, document.querySelector('#age').value);
-  let ages = person.yearsUntil(birthday);
+  let ages = person.yearsUntil(futurebirthday);
   ages = {
     'earth': ages[0],
     'mercury': ages[1],
@@ -30,11 +39,13 @@ function handleYearsUntil(event) {
     'mars': ages[3],
     'jupiter': ages[4]
   };
-  ages = Object.entries(ages);
-  ages.forEach(function (age) {
-    document.querySelector(`#${age[0]}`).innerHTML = age[1];
+
+  Object.entries(ages).forEach(function ([planet, age]) {
+    document.querySelector(`#${planet}Until`).innerHTML = age;
   });
 
+  document.querySelector(`span#futurebirthday`).innerHTML = futurebirthday;
+  document.querySelector('ul#years-until').classList.remove('hidden');
 }
 
 
@@ -42,9 +53,8 @@ function handleYearsUntil(event) {
 function handleFormSubmit(event) {
   event.preventDefault();
   const name = document.querySelector('#name').value;
-  const age = document.querySelector('#age').value;
-  const yearsBetween = document.querySelector('button#birthday');
-  const yearsUntil = document.querySelector('button#years-until');
+  const age = parseInt(document.querySelector('#age').value);
+
   const person = new Person(name, age);
   let ages =
   {
@@ -54,19 +64,31 @@ function handleFormSubmit(event) {
     'jupiter': person.toJupiter()
   };
 
-  ages = Object.entries(ages);
-  ages.forEach(function (age) {
-    document.querySelector(`#${age[0]}`).innerHTML = age[1];
+  Object.entries(ages).forEach(function ([planet, age]) {
+    document.querySelector(`#${planet}Age`).innerHTML = age;
   });
 
-  yearsBetween.addEventListener('click', handleYearsBetween);
-  yearsUntil.addEventListener('click', handleYearsUntil);
+
+  document.querySelector('#age-form').classList.add('hidden');
+  document.querySelector('#results').classList.remove('hidden');
 
 }
 
 window.addEventListener('DOMContentLoaded', () => {
+  const birthdayForm = document.querySelector('form#birthday-form');
+  const futureBirthdayForm = document.querySelector('form#future-birthday-form');
+
   const form = document.querySelector('form#age-form');
   form.addEventListener('submit', handleFormSubmit);
+
+  birthdayForm.addEventListener('submit', handleYearsBetween);
+  futureBirthdayForm.addEventListener('submit', handleYearsUntil);
+
+  document.querySelector('button#birthday').addEventListener('click', () => {
+    document.querySelector('div#years-between').classList.remove('hidden');
+  });
+  document.querySelector('button#years-until').addEventListener('click', () => {
+    document.querySelector('div#years-until').classList.remove('hidden');
+  });
+
 });
-
-
