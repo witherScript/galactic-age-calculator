8f1ad93383a5092d1d0ba76e7003bc409b07a0e7
Genesis Scott <chandlerscott123@gmail.com>
2023-07-27 21:16:25 -0500

    implement UI functionality for calling yearsBetween and yearsUntil methods

--- src/index.js
+++ src/index.js
@@ -3,11 +3,48 @@ import './css/styles.css';
 import 'bootstrap';
 
 
+function handleYearsBetween(event) {
+  event.preventDefault();
+  document.querySelector('div#years-between').classList.remove('hidden');
+  const birthday1 = document.querySelector('#birthday1').value;
+  const birthday2 = document.querySelector('#birthday2').value;
+  const person = new Person(document.querySelector('#name').value, document.querySelector('#age').value);
+  let ages = person.yearsBetween(birthday1, birthday2);
+  ages = Object.entries(ages);
+  ages.forEach(function (age) {
+    document.querySelector(`#${age[0]}`).innerHTML = age[1];
+  });
+}
+
+function handleYearsUntil(event) {
+  event.preventDefault();
+  document.querySelector('div#years-until').classList.remove('hidden');
+  document.querySelector('ul#years-until').classList.remove('hidden');
+  const birthday = document.querySelector('#birthday').value;
+  const person = new Person(document.querySelector('#name').value, document.querySelector('#age').value);
+  let ages = person.yearsUntil(birthday);
+  ages = {
+    'earth': ages[0],
+    'mercury': ages[1],
+    'venus': ages[2],
+    'mars': ages[3],
+    'jupiter': ages[4]
+  };
+  ages = Object.entries(ages);
+  ages.forEach(function (age) {
+    document.querySelector(`#${age[0]}`).innerHTML = age[1];
+  });
+
+}
+
+
+
 function handleFormSubmit(event) {
   event.preventDefault();
   const name = document.querySelector('#name').value;
   const age = document.querySelector('#age').value;
-
+  const yearsBetween = document.querySelector('button#birthday');
+  const yearsUntil = document.querySelector('button#years-until');
   const person = new Person(name, age);
   let ages =
   {
@@ -22,7 +59,8 @@ function handleFormSubmit(event) {
     document.querySelector(`#${age[0]}`).innerHTML = age[1];
   });
 
-  document.querySelector('div#results').classList.remove('hidden');
+  yearsBetween.addEventListener('click', handleYearsBetween);
+  yearsUntil.addEventListener('click', handleYearsUntil);
 
 }
 
